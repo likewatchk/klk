ec7c4469e5742ed2172f42fd76ec2df197c0186b
ksh24865 <ksh24865@github.com>
2019-11-25 16:09:49 +0000

    updated Heartrate and socketClient

---
+++ Codes/HeartRate_sensor/HeartrateClient.py
@@ -0,0 +1,26 @@
+import socket
+from pulsesensor import Pulsesensor
+import time
+import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+import time
+#import threading
+p = Pulsesensor()
+p.startAsyncBPM()
+#Pick an open Port (1000+ recommended), must match the server port
+HOST = '10.27.6.29' 
+# Enter IP or Hostname of your server
+PORT = 12345
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+print("connected")
+
+#Lets loop awaiting for your input
+while True:
+    bpm = p.BPM
+    print(bpm)
+    command = str(bpm)
+    command = command.encode(encoding='UTF-8')
+    time.sleep(1)
+    s.send(command)
+    
