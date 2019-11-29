7301c7a058360423e2c7cf0b392b9b96c4a0ff80
ksh24865 <8819889@naver.com>
2019-11-29 16:03:44 +0900

    plus tkinter

--- Codes/Socket_communication/ServerGsr.py
+++ Codes/Socket_communication/ServerGsr.py
@@ -1,20 +1,73 @@
+import threading
 import socket
+import tkinter
 import spidev
 import RPi.GPIO as GPIO
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.legend as legend
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import time
-import threading
+
 
 def read_spi_adc(adcChannel):
     adcValue=0
     buff=spi.xfer2([6|(adcChannel&4)>>2,(adcChannel&3)<<6,0])
     adcValue=((buff[1]&15)<<8)+buff[2]
     return adcValue
+
+
+def lie_detector_core():
+    global conn, spi, line_max, empty_value, lstx, lst_gsr, lst_pul, fig, ax1, ax2, idx, count
+    
+    data = conn.recv(1024)
+    data = float(data.decode(encoding='UTF-8')[:11])
+    adcValue=read_spi_adc(0)
+    print(adcValue,data)
+    lst_gsr.append(adcValue)
+    lst_pul.append(data)
+    lstx.append(idx)
+    #lstx2.append(i)
+    
+    idx=idx+1
+    lst_len=len(lstx)
+    sum_gsr = 0
+    sum_pul = 0
+    st_num=0
+    data_count = 1
+    ed_num=lst_len
+    if lst_len >30 :
+        st_num=lst_len-30
+        data_count=30
+        del lstx[0]
+        del lst_gsr[0]
+        del lst_pul[0]
+    else:
+        st_num=0
+        data_count=lst_len
+    for i in range(st_num,ed_num):
+        sum_gsr = sum(lst_gsr)
+        sum_pul = sum(lst_pul)
+    avg_gsr = sum_gsr/data_count
+    avg_pul = sum_pul/data_count
+    ax1.cla()
+    ax2.cla()
+    ax1.plot(lstx,lst_gsr,'r.-')
+    ax2.plot(lstx,lst_pul,'b.-')
+    fig.canvas.draw()
+    lie_detector.after(200, lie_detector_core)
+    
+    
+
 #---
 
-HOST = '10.27.6.29' 
+lie_detector = tkinter.Tk()
+lie_detector.title("Lie Detector")
+lie_detector.geometry("400x400+40+40")
+lie_detector.resizable(False, False)
+
+
+HOST = '10.27.7.17' 
 # Server IP or Hostname
 PORT = 12345 
 # Pick an open Port (1000+ recommended), must match the client sport
@@ -30,8 +83,11 @@ print ('Socket awaiting messages')
 (conn, addr) = s.accept()
 print ('Connected')
 
-#---
 
+#------
+
+idx = 0
+count=0
 spi = spidev.SpiDev()
 spi.open(0,0)
 spi.max_speed_hz=500000
@@ -44,44 +100,15 @@ matplotlib.use("TKAgg")
 fig=plt.gcf()
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
-fig.show()
-fig.canvas.draw()
-plt.pause(0.01)
-time.sleep(0.5)
-i=0
-count=0
-while True:
-    data = conn.recv(1024)
-    data = float(data.decode(encoding='UTF-8'))
-    adcValue=read_spi_adc(0)
-    print(adcValue,data)
-    lst_gsr.append(adcValue)
-    lst_pul.append(data)
-    lstx.append(i)
-    #lstx2.append(i)
-    
-    i=i+1
-    lst_len=len(lstx)
-    sum=0
-    st_num=0
-    data_count=1
-    ed_num=lst_len
-    if lst_len >10:
-        st_num=lst_len-10
-        data_count=10
-    else:
-        st_num=0
-        data_count=lst_len
-    for i in range(st_num,ed_num):
-        sum=sum+lst_gsr[i]
-    avg= sum/data_count
-    plt.ylim(0,4096)
-    ax1.plot(lstx,lst_gsr,'r.-')
-    plt.ylim(0,300)
-    ax2.plot(lstx,lst_pul,'r.-')
-    fig.canvas.draw()
-    plt.pause(0.01)
-    time.sleep(0.2)
-    
+
+detector_canvas = FigureCanvasTkAgg(fig, master=lie_detector)
+detector_canvas.draw()
+detector_canvas.get_tk_widget().pack(expand=True, fill="both")
+
+lie_detector.after(200, lie_detector_core)
+lie_detector.mainloop()
+
+print("?????????")
+
 GPIO.cleanup()
 exit()
